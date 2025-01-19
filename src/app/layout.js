79050cbc42af30e043c827bac74e5504f832c9e8
Watcharit Tuntayakul <watcharit.new@gmail.com>
2025-01-19 14:46:32 +0700

    feat: align format

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,6 @@ import {
 import { QueryProvider } from "@/lib/providers";
 import { AppLayout } from "@/components";
 
-import Image from "next/image";
 import { Kanit } from "next/font/google";
 import "./globals.css";
 
@@ -32,39 +31,6 @@ export default function RootLayout({ children }) {
       <body
         className={`${kanit.className} min-w-[90rem] font-normal text-base bg-gray-100`}
       >
-        <div className="w-full h-20 bg-white  flex items-center px-4 pr-8 relative z-50">
-          <div className="flex-grow">
-            <div className="flex flex-row items-center gap-2">
-              <Image
-                src="/termtem-logo.svg"
-                alt="termtem logo"
-                width={56}
-                height={56}
-                priority
-              />
-              <div className="flex flex-col">
-                <p className="font-medium text-gray-600 text-lg">
-                  กรมป้องกันและบรรเทาสาธารณภัย กระทรวงมหาดไทย
-                </p>
-                <p className="text-gray-400 text-sm">
-                  Department of Disaster Prevention and Mitigation. Ministry of
-                  Interior. Thailand
-                </p>
-              </div>
-            </div>
-          </div>
-
-          <div className="flex-none">
-            <div className="flex flex-row gap-4">
-              <Image src="/bell.svg" alt="bell" width={29} height={30} />
-              <Image src="/profile.svg" alt="profile" width={56} height={56} />
-              <div className="flex flex-col text-black">
-                <p>Admin</p>
-                <p>ผู้ว่าราชการจังหวัด</p>
-              </div>
-            </div>
-          </div>
-        </div>
         <QueryProvider>
           <MantineProvider>
             <AppLayout>{children}</AppLayout>
