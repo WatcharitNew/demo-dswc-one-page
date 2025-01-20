7c9fe19cafeb9625ce6150eda9c7f144ba2fe42d
Watcharit Tuntayakul <watcharit.new@gmail.com>
2025-01-20 21:28:58 +0700

    chore: split constant file

---
+++ src/constants/index.js
@@ -0,0 +1,39 @@
+export const DISASTERS = [
+  {
+    srcName: "disaster-flood",
+    text: "อุทกภัย",
+    value: "flood",
+  },
+  {
+    srcName: "disaster-drought",
+    text: "ภัยแล้ง",
+    value: "drought",
+  },
+  {
+    srcName: "disaster-storm",
+    text: "วาตภัย/พายุ",
+    value: "storm",
+  },
+  {
+    srcName: "disaster-mudslide",
+    text: "ดินโคลนถล่ม",
+    value: "mudslide",
+  },
+  {
+    srcName: "disaster-earthquake-tsunami",
+    text: "แผ่นดินไหว",
+    subtext: "สึนามิ",
+    value: "earthquake-tsunami",
+  },
+  {
+    srcName: "disaster-pm25",
+    text: "หมอกควัน",
+    subtext: "PM2.5",
+    value: "pm25",
+  },
+  {
+    srcName: "disaster-forest-fire",
+    text: "ไฟป่า",
+    value: "forest-fire",
+  },
+];
