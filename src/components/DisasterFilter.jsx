7cbc6e9f3a532e280c2fae91b629bb88e607a244
Watcharit Tuntayakul <42472081+WatcharitNew@users.noreply.github.com>
2025-01-20 21:30:50 +0700

    feat: home disaster filter (#4)
    * feat: setup layout
    
    * chore: fix text
    
    * feat: align format
    
    * chore: add disaster images
    
    * feat: add components disaster filter and label
    
    * feat: adjust component to page
    
    * chore: custom style to tailwind
    
    * chore: add package clsx
    
    * chore: split constant file
    
    * fix: move client render to component

---
+++ src/components/DisasterFilter.jsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { Image } from "@mantine/core";
+import { useState } from "react";
+
+import { DisasterLabel } from ".";
+
+export function DisasterFilter({ disasters = [] }) {
+  const [selectedDisaster, setSelectedDisaster] = useState("flood");
+
+  return (
+    <div className="w-full h-full flex flex-col items-center">
+      <div className="max-w-[78.875rem] relative mt-7">
+        <Image
+          src="/disaster-bg.svg"
+          alt="disaster background"
+          width={1262}
+          height={276}
+        />
+        <div className="absolute top-8 left-56">
+          <p className="text-white text-2xl font-medium">
+            รูปแบบรายงานสาธารณภัยที่แนะนำ
+          </p>
+          <div className="flex flex-row flex-shrink mt-4 h-[10.489rem] items-center">
+            {disasters.map((disaster) => (
+              <DisasterLabel
+                key={disaster.srcName}
+                {...disaster}
+                isSelected={selectedDisaster === disaster.value}
+                onClick={setSelectedDisaster}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
