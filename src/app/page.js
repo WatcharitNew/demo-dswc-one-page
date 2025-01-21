dc90d88785d3ec6efab5384cfbc0fd3fe2342717
Narawit Sukwattanajaroon <105602329+Narawit-S@users.noreply.github.com>
2025-01-22 00:19:13 +0700

    feat: move main container to home

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <div className="px-[90px]"></div>;
+  return (
+    <div className="w-full flex flex-col items-center pt-7">
+      <div className="max-w-[78.875rem]">
+        <DisasterFilter disasters={DISASTERS} />
+      </div>
+    </div>
+  );
 }
