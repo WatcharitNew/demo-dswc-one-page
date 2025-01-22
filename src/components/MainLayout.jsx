d0a37402a022efc853966ed4edfd6110f4003c09
Watcharit Tuntayakul <watcharit.new@gmail.com>
2025-01-23 00:33:56 +0700

    fix: add useEffect on redirect to login

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -1,16 +1,20 @@
 "use client";
 
 import { AppLayout } from "@/components";
-import { useAuthContext } from "@/lib/providers/auth";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+import { useAuthContext } from "@/lib/providers/auth";
 
 export default function MainLayout({ children }) {
   const { isAuth, isLoading } = useAuthContext();
   const router = useRouter();
 
-  if (!isAuth && !isLoading) {
-    router.push("/login");
-  }
+  useEffect(() => {
+    if (!isAuth && !isLoading) {
+      router.push("/login");
+    }
+  }, [isAuth, isLoading, router]);
 
   return <AppLayout>{children}</AppLayout>;
 }
