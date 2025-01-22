e0c1381a2acc9be7da801686eaecf098adb2b8d1
Narawit Sukwattanajaroon <105602329+Narawit-S@users.noreply.github.com>
2025-01-22 23:54:59 +0700

    feat: report scale up modal

--- src/containers/ReportListContainer.jsx
+++ src/containers/ReportListContainer.jsx
@@ -1,9 +1,29 @@
 "use client";
 
+import { useState, useCallback } from "react";
 import { Input, Image } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+
+import { MOCK_REPORT, MOCK_REPORT_CENTER } from "@/constants";
+
+import { ReportScaleUpModal } from "@/components";
 import { Search } from "@/icons";
 
 export const ReportListContainer = () => {
+  const [selectedReport, setSelectedReport] = useState({
+    imageSrc: "",
+    templateName: "",
+  });
+  const [opened, { open, close }] = useDisclosure(false);
+
+  const handleOnClickReport = useCallback(
+    (selected) => {
+      setSelectedReport(selected);
+      open();
+    },
+    [open]
+  );
+
   return (
     <div className="flex flex-col mt-6 gap-6">
       <div className="flex flex-row items-center justify-between">
@@ -18,65 +38,62 @@ export const ReportListContainer = () => {
         />
       </div>
       <div className="flex flex-col gap-2">
-        <p className="font-medium text-xl">รูปแบบรายงานส่วนกลาง (2)</p>
+        <p className="font-medium text-xl">
+          รูปแบบรายงานส่วนกลาง ({MOCK_REPORT_CENTER.length})
+        </p>
         <div className="flex items-center gap-6">
-          <div className="report-card-container">
-            <div className="report-card-inner-container">
-              <div className="grow">
-                <Image
-                  h={240}
-                  src="https://placehold.co/200x400?text=Placeholder"
-                />
+          {MOCK_REPORT_CENTER.map((report) => (
+            <div
+              key={report.name}
+              className="report-card-container"
+              onClick={() =>
+                handleOnClickReport({
+                  imageSrc: report.src,
+                  tamplateName: report.name,
+                })
+              }
+            >
+              <div className="report-card-inner-container">
+                <div className="grow">
+                  <Image h={240} src={report.src} />
+                </div>
+                <p className="text-lg font-medium">{report.name}</p>
               </div>
-              <p className="text-lg font-medium">TEM_DDPM_002</p>
             </div>
-          </div>
-          <div className="report-card-container">
-            <div className="report-card-inner-container">
-              <div className="grow">
-                <Image
-                  h={240}
-                  src="https://placehold.co/200x400?text=Placeholder"
-                />
-              </div>
-              <p className="text-lg font-medium">TEM_DDPM_001</p>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
+
       {/* Mock */}
-      <div className="flex flex-col gap-2">
-        <p className="font-medium text-xl">รอตรวจสอบ (1)</p>
-        <div className="flex items-center gap-4">
-          <div className="report-card-container">
-            <div className="report-card-inner-container">
-              <div className="grow">
-                <Image
-                  h={240}
-                  src="https://placehold.co/200x400?text=Placeholder"
-                />
+      {MOCK_REPORT.map((report) => (
+        <div key={report.title} className="flex flex-col gap-2">
+          <p className="font-medium text-xl">{report.title}</p>
+          <div className="flex items-center gap-4">
+            <div
+              className="report-card-container"
+              onClick={() =>
+                handleOnClickReport({
+                  imageSrc: report.src,
+                  templateName: report.name,
+                })
+              }
+            >
+              <div className="report-card-inner-container">
+                <div className="grow">
+                  <Image h={240} src={report.src} />
+                </div>
+                <p className="text-lg font-medium">{report.name}</p>
               </div>
-              <p className="text-lg font-medium">TEM_DDPM_003</p>
             </div>
           </div>
         </div>
-      </div>
-      <div className="flex flex-col gap-2">
-        <p className="font-medium text-xl">ฉบับร่าง (1)</p>
-        <div className="flex items-center gap-4">
-          <div className="report-card-container">
-            <div className="report-card-inner-container">
-              <div className="grow">
-                <Image
-                  h={240}
-                  src="https://placehold.co/200x400?text=Placeholder"
-                />
-              </div>
-              <p className="text-lg font-medium">V01_Draft</p>
-            </div>
-          </div>
-        </div>
-      </div>
+      ))}
+
+      <ReportScaleUpModal
+        selectedReport={selectedReport}
+        opened={opened}
+        onClose={close}
+      />
     </div>
   );
 };
