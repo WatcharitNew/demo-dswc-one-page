f7f196c486e3d251d572ebec2923e1982720c9ad
Narawit Sukwattanajaroon <105602329+Narawit-S@users.noreply.github.com>
2025-01-23 23:33:14 +0700

    feat: redirect to templater

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,5 @@
-import { DISASTERS } from "@/constants";
-
-import { DisasterFilter } from "@/components/DisasterFilter";
-import { ReportListContainer } from "@/containers";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div className="w-full flex flex-col items-center py-8">
-      <div className="max-w-[78.875rem]">
-        <DisasterFilter disasters={DISASTERS} />
-
-        <ReportListContainer/>
-      </div>
-    </div>
-  );
+  redirect('/templater')
 }
