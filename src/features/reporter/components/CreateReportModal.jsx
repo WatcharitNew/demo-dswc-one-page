fbb93a11f18304c263e378c658b806da61f6ab5e
Narawit Sukwattanajaroon <105602329+Narawit-S@users.noreply.github.com>
2025-01-25 01:09:31 +0700

    feat: tab to select disaster type on create report

--- src/features/reporter/components/CreateReportModal.jsx
+++ src/features/reporter/components/CreateReportModal.jsx
@@ -1,8 +1,27 @@
 "use client";
 
-import { Modal } from "@mantine/core";
+import { useState } from "react";
+import { DISASTERS_REPORT_SELECT } from "../constants";
 
-export const CreateReportModal = ({opened, onClose}) => {
+import {
+  CloseButton,
+  Flex,
+  FloatingIndicator,
+  Modal,
+  Tabs,
+} from "@mantine/core";
+
+import classes from "./tab.module.css";
+
+export const CreateReportModal = ({ opened, onClose }) => {
+  const [rootRef, setRootRef] = useState(null);
+  const [value, setValue] = useState("all");
+  const [controlsRefs, setControlsRefs] = useState({});
+  const setControlRef = (val) => (node) => {
+    controlsRefs[val] = node;
+    setControlsRefs(controlsRefs);
+  };
+
   return (
     <Modal
       opened={opened}
@@ -21,7 +40,41 @@ export const CreateReportModal = ({opened, onClose}) => {
       size="70%"
       centered
     >
-      test
+      <Flex className="items-center justify-between">
+        <p className="text-2xl font-medium">รูปแบบรายงานสาธารณะภัย</p>
+        <CloseButton onClick={onClose} />
+      </Flex>
+
+      <Tabs
+        variant="none"
+        orientation="vertical"
+        className="mt-6"
+        value={value}
+        onChange={setValue}
+      >
+        <Tabs.List ref={setRootRef} className={classes.list}>
+          {DISASTERS_REPORT_SELECT.map((disaster) => (
+            <Tabs.Tab
+              key={disaster.value}
+              value={disaster.value}
+              ref={setControlRef(disaster.value)}
+              className={classes.tab}
+              styles={{ tabLabel: { display: "flex", alignItems: 'center', gap: '1rem' } }}
+            >
+              <disaster.icon />
+              {disaster.text}
+            </Tabs.Tab>
+          ))}
+
+          <FloatingIndicator
+            target={value ? controlsRefs[value] : null}
+            parent={rootRef}
+            className={classes.indicator}
+          />
+        </Tabs.List>
+
+        <div className="w-full">test</div>
+      </Tabs>
     </Modal>
   );
 };
--- src/features/reporter/constants/index.js
+++ src/features/reporter/constants/index.js
@@ -1,3 +1,5 @@
+import { Search } from "@/icons";
+
 export const MOCK_REPORT_LIST = [
   {
     id: '1',
@@ -32,3 +34,46 @@ export const MOCK_REPORT_LIST = [
     status: 'อนุมัติ',
   },
 ]
+
+export const DISASTERS_REPORT_SELECT = [
+  {
+    text: "ทั้งหมด",
+    value: "all",
+    icon: Search
+  },
+  {
+    text: "อุทกภัย",
+    value: "flood",
+    icon: Search
+  },
+  {
+    text: "ภัยแล้ง",
+    value: "drought",
+    icon: Search
+  },
+  {
+    text: "วาตภัย/พายุ",
+    value: "storm",
+    icon: Search
+  },
+  {
+    text: "ดินโคลนถล่ม",
+    value: "mudslide",
+    icon: Search
+  },
+  {
+    text: "แผ่นดินไหว/สึนามิ",
+    value: "earthquake-tsunami",
+    icon: Search
+  },
+  {
+    text: "หมอกควัน/PM2.5",
+    value: "pm25",
+    icon: Search
+  },
+  {
+    text: "ไฟป่า",
+    value: "forest-fire",
+    icon: Search
+  },
+];
