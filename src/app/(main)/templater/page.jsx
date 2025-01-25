6b037085dce1d841ba11feb16c5dd69aa15e210a
Watcharit Tuntayakul <watcharit.new@gmail.com>
2025-01-25 11:01:34 +0700

    chore: move file

--- src/app/(main)/templater/page.jsx
+++ src/app/(main)/templater/page.jsx
@@ -1,32 +1,16 @@
-"use client";
-import { Button, Input } from "@mantine/core";
-import { useDisclosure } from "@mantine/hooks";
-import { DisasterFilter, TemplateModal } from "@/components";
 import { DISASTERS } from "@/constants";
-import { AddIcon, SearchIcon } from "@/icons";
 
-export default function Home() {
-  const [opened, { open, close }] = useDisclosure(false);
+import { DisasterFilter } from "@/components/DisasterFilter";
+import { ReportListContainer } from "@/containers";
+
+export default function Templater() {
   return (
-    <div className="px-24">
-      <DisasterFilter disasters={DISASTERS} />
-      <div className="row justify-between my-6">
-        <Input
-          leftSection={<SearchIcon size={16} />}
-          placeholder="ค้นหาด้วย ชื่อ Template"
-          className="w-[435px]"
-          size="md"
-        />
-        <Button
-          className="w-40 h-11"
-          variant="primary"
-          leftSection={<AddIcon />}
-          onClick={open}
-        >
-          สร้างใหม่
-        </Button>
+    <div className="w-full flex flex-col items-center py-8">
+      <div className="max-w-[78.875rem]">
+        <DisasterFilter disasters={DISASTERS} />
+
+        <ReportListContainer/>
       </div>
-      <TemplateModal opened={opened} close={close} />
     </div>
   );
 }
