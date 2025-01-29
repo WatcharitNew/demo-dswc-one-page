1946a53d3fe1a00a50a1d51d0157bf4bccc2c628
Watcharit Tuntayakul <watcharit.new@gmail.com>
2025-01-29 21:40:36 +0700

    feat: add service get list templates

---
+++ src/features/templater/services/getListTemplates.jsx
@@ -0,0 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { fetchAPI } from "@/lib/api";
+import { PROVINCES } from "@/constants";
+import { LIST_TEMPLATES } from "../constants/queryKeys";
+
+const getListTemplates = async (province) => {
+  const provinceId = PROVINCES.findIndex((p) => p === province);
+  console.log(provinceId)
+
+  const { data } = await fetchAPI({
+    path: `/getListTemplates?province_id=${provinceId}`,
+    method: "GET",
+  });
+
+  return data
+};
+
+export const useListTemplates = (province) => {
+  return useQuery({
+    queryKey: [LIST_TEMPLATES, province],
+    queryFn: () => getListTemplates(province),
+  });
+};
