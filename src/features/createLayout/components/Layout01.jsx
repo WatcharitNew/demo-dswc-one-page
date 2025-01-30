6c56187dfc2d10d2d1c062a8e1b7f8fd66268756
dolwijit.j <dolwijit.j@gmail.com>
2025-01-31 02:26:11 +0900

    [DDH-32] Adjust size

--- src/features/createLayout/components/Layout01.jsx
+++ src/features/createLayout/components/Layout01.jsx
@@ -7,12 +7,12 @@ const Layout01 = () => {
     <div className="flex flex-col w-full max-w-7xl max-h-full mx-auto p-4 gap-1">
       <div className="flex flex-col gap-1 basis-[13%] w-full bg-gray-300 p-1">
         <div className="flex gap-1 basis-[75%]">
-          <LayoutComponent className='basis-[10%]' />
-          <div className="basis-[80%] flex flex-col gap-1">
+          <LayoutComponent className='basis-[13%]' />
+          <div className="basis-[74%] flex flex-col gap-1">
             <LayoutComponent className='basis-[50%] bg-inherit' />
             <LayoutComponent className='basis-[50%]' />
           </div>
-          <LayoutComponent className='basis-[10%]' />
+          <LayoutComponent className='basis-[13%]' />
         </div>
         <div className="flex gap-1 basis-[25%]">
           <LayoutComponent className='w-full' />
