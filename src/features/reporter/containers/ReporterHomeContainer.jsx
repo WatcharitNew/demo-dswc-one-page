94b58fbc1adf3fc2efbb24504c26c1dca63b5edf
Narawit Sukwattanajaroon <105602329+Narawit-S@users.noreply.github.com>
2025-02-03 20:52:40 +0700

    fix: provice data turn to object (#27)

--- src/features/reporter/containers/ReporterHomeContainer.jsx
+++ src/features/reporter/containers/ReporterHomeContainer.jsx
@@ -68,7 +68,7 @@ export const ReporterHomeContainer = () => {
           <div className="col gap-4">
             <div className="row items-center justify-between">
               <p className="text-xl font-medium">
-                ประวัติการจัดทำรายงานสาธารณภัยประจำวัน จังหวัด{data?.province}
+                ประวัติการจัดทำรายงานสาธารณภัยประจำวัน จังหวัด{data?.province?.name_th}
               </p>
               <UnstyledButton className="row items-center text-sm gap-2 text-gray-900">
                 <Sort className="size-6 text-gray-600" />
