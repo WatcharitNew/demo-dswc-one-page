c5b9b138ea0669c7c1b7bf34268efa4fbcf28d1d
Narawit Sukwattanajaroon <105602329+Narawit-S@users.noreply.github.com>
2025-02-04 23:28:30 +0700

    feat: button ui in layout component

--- src/features/createLayout/components/LayoutComponent.jsx
+++ src/features/createLayout/components/LayoutComponent.jsx
@@ -1,9 +1,12 @@
 "use client";
 
 import { useMemo, useCallback, useContext } from "react";
-import { Image } from "@mantine/core";
+import { clsx } from "clsx";
+import { Button, Image } from "@mantine/core";
 
 import { CreateLayoutContext } from "@/contexts/CreateLayoutContext";
+
+import { PencilIcon } from "@/icons";
 
 const LayoutComponent = ({ idx, style }) => {
   const { openTemplateComponentModal, setCurrentBoxId, createLayoutData } =
@@ -25,11 +28,30 @@ const LayoutComponent = ({ idx, style }) => {
     <div
       onClick={onLayoutClick}
       style={style}
-      className={`absolute flex items-center justify-center hover:outline hover:outline-blue-500 cursor-pointer
-        ${darkGrayIndex == idx ? "bg-gray2-400" : "bg-gray-200"}
-      `}
+      className={clsx(
+        "absolute flex items-center justify-center hover:outline hover:outline-blue-500 cursor-pointer hover:bg-white",
+        darkGrayIndex === idx ? "bg-gray2-400" : "bg-gray-200",
+        imgSrc ? "box-parent-edit" : "box-parent-add"
+      )}
     >
-      {imgSrc ? <Image className="w-full h-full" src={imgSrc} fit="contain"/> : "+"}
+      {imgSrc ? (
+        <Image className="w-full h-full" src={imgSrc} fit="contain" />
+      ) : (
+        "+"
+      )}
+      <Button
+        className="box-button-add button-in-layout"
+        variant="outline"
+      >
+        + เพิ่ม
+      </Button>
+      <Button
+        className="box-button-edit button-in-layout top-1 right-1"
+        leftSection={<PencilIcon size={14} color="#1473e6"/>}
+        variant="outline"
+      >
+        แก้ไข
+      </Button>
     </div>
   );
 };
