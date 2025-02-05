1b25d451bb5398d2b39afbcc978ca3ef73f272a6
Watcharit Tuntayakul <watcharit.new@gmail.com>
2025-02-05 23:33:54 +0700

    feat: add style hover on custom component

--- src/features/reporter/components/ReporterCustomizableComponent.jsx
+++ src/features/reporter/components/ReporterCustomizableComponent.jsx
@@ -1,24 +1,15 @@
 "use client";
 
+import clsx from "clsx";
 import { TEMPLATE_HEIGHT, TEMPLATE_WIDTH } from "@/constants";
-import { Image } from "@mantine/core";
+import { Button, Image } from "@mantine/core";
+import { PencilIcon } from "@/icons";
 
 export const ReporterCustomizableComponent = ({ box, onClick, imgUrl }) => {
-  return imgUrl ? (
-    <Image
-      src={imgUrl}
-      className="absolute"
-      style={{
-        height: `calc(${box.bottom - box.top}*${TEMPLATE_HEIGHT}px)`,
-        width: `calc(${box.right - box.left}*${TEMPLATE_WIDTH}px)`,
-        top: `calc(${box.top}*${TEMPLATE_HEIGHT}px)`,
-        left: `calc(${box.left}*${TEMPLATE_WIDTH}px)`,
-      }}
-    />
-  ) : (
+  return (
     <div
       onClick={onClick}
-      className="absolute flex items-center justify-center hover:outline hover:outline-blue-500 cursor-pointer bg-gray-200"
+      className={clsx("absolute flex items-center justify-center hover:outline hover:outline-blue-500 cursor-pointer bg-gray-200", imgUrl ? "box-parent-edit" : "box-parent-add")}
       style={{
         height: `calc(${box.bottom - box.top}*${TEMPLATE_HEIGHT}px)`,
         width: `calc(${box.right - box.left}*${TEMPLATE_WIDTH}px)`,
@@ -26,7 +17,24 @@ export const ReporterCustomizableComponent = ({ box, onClick, imgUrl }) => {
         left: `calc(${box.left}*${TEMPLATE_WIDTH}px)`,
       }}
     >
-      +
+      {imgUrl ? (
+        <Image className="w-full h-full" src={imgUrl} fit="contain" />
+      ) : (
+        "+"
+      )}
+      <Button
+        className="box-button-add button-in-layout"
+        variant="outline"
+      >
+        + เพิ่ม
+      </Button>
+      <Button
+        className="box-button-edit button-in-layout top-1 right-1"
+        leftSection={<PencilIcon size={14} color="#1473e6"/>}
+        variant="outline"
+      >
+        แก้ไข
+      </Button>
     </div>
-  );
+  )
 };
