2159133f96e2d8c5d0441a2633b65cd7c8e27cb0
Watcharit Tuntayakul <watcharit.new@gmail.com>
2025-02-05 18:13:32 +0700

    feat: add custom component on report

--- src/features/reporter/containers/ReporterCreateContainer.jsx
+++ src/features/reporter/containers/ReporterCreateContainer.jsx
@@ -1,128 +1,191 @@
 "use client";
 
-import { useEffect, useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useParams } from "next/navigation";
 import { useDisclosure } from "@mantine/hooks";
 
 import { useReloadReport } from "../services";
 
-import { RefreshReportModal } from "../components";
+import { RefreshReportModal, ReporterComponentModal } from "../components";
 import { Button, Image } from "@mantine/core";
 import { Refresh, Multiplier1, Multiplier15, Multiplier2 } from "@/icons";
 import { useAuthContext } from "@/lib/providers/auth";
-import { PROVINCES, TEMPLATE_HEIGHT, TEMPLATE_WIDTH } from "@/constants";
-
-const getProvinceId = (province) => {
-  return PROVINCES.findIndex((p) => p === province)
-}
+import { ReporterCustomizableComponent } from "../components/ReporterCustomizableComponent";
+import { usePostGenComponents } from "@/services";
+import dayjs from "dayjs";
 
 const getInitialReloadParams = (id, data) => {
   return {
     template_id: id,
-    province_id: getProvinceId(data?.province),
-    date: data?.date,
-    customizable_component: []
-  }
-}
+    province_id: data?.province?.id,
+    date: data?.date ? dayjs(data?.date).format("YYYY-MM-DD") : undefined,
+    customizable_component: [],
+  };
+};
 
 export const ReporterCreateContainer = () => {
   const { id } = useParams();
-  const { data } = useAuthContext()
+  const { data } = useAuthContext();
   const [opened, { open, close }] = useDisclosure(false);
-  const { mutate: reload, data: reloadedReport } = useReloadReport()
+  const { mutate: reload, data: reloadedReport } = useReloadReport();
+  const [customComponents, setCustomComponents] = useState(undefined);
+  const [component, setComponent] = useState(undefined);
+  const [content, setContent] = useState();
+
+  const [
+    openedCustomModal,
+    { open: openCustomModal, close: closeCustomModal },
+  ] = useDisclosure(false);
+  const { mutate: postGenComponents } = usePostGenComponents();
 
   const imageSrc = useMemo(() => {
-    return reloadedReport?.data?.img_url
-  }, [reloadedReport?.data])
+    return reloadedReport?.data?.img_url;
+  }, [reloadedReport?.data]);
 
-  const reloadReport = () => {
-    open()
-    reload(getInitialReloadParams(id, data), {
-      onSettled() {
-        close();
+  const reloadReport = (options = {}) => {
+    open();
+    reload(
+      { ...getInitialReloadParams(id, data), ...options },
+      {
+        onSettled() {
+          close();
+        },
+      }
+    );
+  };
+
+  const getGenComponentPayload = () => {
+    const params = {
+      component_id: component?.component_id,
+      province_id: data?.province.id,
+      date: data?.date ? dayjs(data?.date).format("YYYY-MM-DD") : undefined,
+      box_id: component?.box?.box_id,
+      layout_id: reloadedReport?.data?.layout?.layout_id,
+      content: component?.customization_type === "text" ? content : undefined,
+    };
+
+    let formFile;
+    if (component?.customization_type === "image" && content) {
+      formFile = new FormData();
+      formFile.append("file_content", content[0]);
+    }
+
+    return {
+      params,
+      file: formFile,
+    };
+  };
+
+  const postGenComponentApi = () => {
+    postGenComponents(getGenComponentPayload(), {
+      onSuccess: (response) => {
+        const componentIdx = customComponents.findIndex((comp) => {
+          return comp.component_id === component.component_id;
+        });
+        setCustomComponents([
+          ...customComponents.slice(0, componentIdx),
+          { ...customComponents[componentIdx], imgUrl: response.img_url },
+          ...customComponents.slice(componentIdx + 1),
+        ]);
+
+        setContent(undefined);
+        closeCustomModal();
       },
+      onError: () => {},
     });
-  }
-
-  const customizableComponents = useMemo(() => {
-    return reloadedReport?.data?.customizable_component || []
-  }, [reloadedReport?.data])
+  };
 
   useEffect(() => {
-    reloadReport()
-  }, [id, data])
+    if (id && data?.province) {
+      reloadReport();
+    }
+  }, [id, data]);
+
+  useEffect(() => {
+    if (customComponents === undefined) {
+      setCustomComponents(reloadedReport?.data?.customizable_component);
+    }
+  }, [reloadedReport?.data]);
 
   return (
-    <div className="col h-full">
-      <div className="grow row justify-center pt-2 pb-8 overflow-auto">
-        <div className="w-full h-fit max-w-[34rem] col gap-2">
-          <div className="row items-end justify-between">
-            <p className="font-medium text-gray-300">ตัวอย่างรายงานสาธารณภัย</p>
-            <Button.Group>
-              <Button
-                className="h-10 px-3 !border-0 !rounded-r-none"
-                variant="default"
-                onClick={() => {
-                  reloadReport()
-                }}
-              >
-                <Refresh />
-              </Button>
-              <Button
-                className="h-10 px-3 !border-0 !rounded-none"
-                variant="default"
-              >
-                <Multiplier1 />
-              </Button>
-              <Button
-                className="h-10 px-3 !border-0 !rounded-none"
-                variant="default"
-              >
-                <Multiplier15 />
-              </Button>
-              <Button
-                className="h-10 px-3 !border-0 !rounded-l-none"
-                variant="default"
-              >
-                <Multiplier2 />
-              </Button>
-            </Button.Group>
-          </div>
-          <div className="relative">
-            <Image
-              alt="report-by-id-image"
-              src={imageSrc}
-              className="w-full"
-              fit="contain"
-            />
-            {customizableComponents.map(({ box, component_id }, idx) => (
-              <div
-                key={`${component_id}-${idx}`}
-                onClick={() => console.log('click box')}
-                className="bg-gray-500 absolute cursor-pointer"
-                style={{
-                  height: `calc(${box.bottom-box.top}*${TEMPLATE_HEIGHT}px)`,
-                  width: `calc(${box.right-box.left}*${TEMPLATE_WIDTH}px)`,
-                  top: `calc(${box.top}*${TEMPLATE_HEIGHT}px)`,
-                  left: `calc(${box.left}*${TEMPLATE_WIDTH}px)`
-                }}
-              ></div>
-            ))}
+    <>
+      <div className="col h-full">
+        <div className="grow row justify-center pt-2 pb-8 overflow-auto">
+          <div className="w-full h-fit max-w-[34rem] col gap-2">
+            <div className="row items-end justify-between">
+              <p className="font-medium text-gray-300">
+                ตัวอย่างรายงานสาธารณภัย
+              </p>
+              <Button.Group>
+                <Button
+                  className="h-10 px-3 !border-0 !rounded-r-none"
+                  variant="default"
+                  onClick={() => reloadReport()}
+                >
+                  <Refresh />
+                </Button>
+                <Button
+                  className="h-10 px-3 !border-0 !rounded-none"
+                  variant="default"
+                >
+                  <Multiplier1 />
+                </Button>
+                <Button
+                  className="h-10 px-3 !border-0 !rounded-none"
+                  variant="default"
+                >
+                  <Multiplier15 />
+                </Button>
+                <Button
+                  className="h-10 px-3 !border-0 !rounded-l-none"
+                  variant="default"
+                >
+                  <Multiplier2 />
+                </Button>
+              </Button.Group>
+            </div>
+            <div className="relative">
+              <Image
+                alt="report-by-id-image"
+                src={imageSrc}
+                className="w-full"
+                fit="contain"
+              />
+              {customComponents?.map((comp, idx) => (
+                <ReporterCustomizableComponent
+                  key={`${comp.component_id}-${idx}`}
+                  box={comp.box}
+                  imgUrl={comp.imgUrl}
+                  onClick={() => {
+                    setComponent(comp);
+                    openCustomModal();
+                  }}
+                />
+              ))}
+            </div>
           </div>
         </div>
-      </div>
-      <div className="bg-white h-16 p-3 row items-center justify-center">
-        <div className="row items-center justify-end gap-4 w-full max-w-[34rem]">
-          <Button className="h-10 min-w-40" variant="outline">
-            แก้ไข
-          </Button>
-          <Button className="h-10 min-w-40" variant="primary">
-            ส่งอนุมัติ
-          </Button>
+        <div className="bg-white h-16 p-3 row items-center justify-center">
+          <div className="row items-center justify-end gap-4 w-full max-w-[34rem]">
+            <Button className="h-10 min-w-40" variant="outline">
+              แก้ไข
+            </Button>
+            <Button className="h-10 min-w-40" variant="primary">
+              ส่งอนุมัติ
+            </Button>
+          </div>
         </div>
-      </div>
 
-      <RefreshReportModal opened={opened} />
-    </div>
+        <RefreshReportModal opened={opened} />
+        <ReporterComponentModal
+          type={component?.customization_type}
+          opened={openedCustomModal}
+          onClose={closeCustomModal}
+          onProcess={postGenComponentApi}
+          content={content}
+          setContent={setContent}
+        />
+      </div>
+    </>
   );
 };
